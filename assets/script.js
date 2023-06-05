119b007b1de00b66135de226530b12cbce52baea
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-06-05 17:13:39 -0400

    added local storage saving functionality

--- assets/script.js
+++ assets/script.js
@@ -31,8 +31,13 @@
 $(document).ready(function () {
   $('.saveBtn').on('click', function () {
     //get nearby values
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
 
+    localStorage.setItem(time, value);
   });
+
+
   function hourUpdater () {
     var currentHour = dayjs().hour();
     //loop over time blocks
@@ -58,6 +63,17 @@ $(document).ready(function () {
 
   hourUpdater();
   
+  setInterval(hourUpdater, 10000);
+//load any saved data from LocalStorage
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 })
 
 var today = dayjs();
